6ecb1cc170c06b33ba663fc5b89d71fbdd7d47eb
Vivek Yadav <vivekyadav1207731111@gmail.com>
2020-03-29 11:27:05 +0530

    Update stm32f407_timer.c

--- stm32f407_timer.c
+++ stm32f407_timer.c
@@ -1,5 +1,5 @@
-
-
+// TODO: make general Time delay funcation.
+//     : Explore other General and Advance Funcation.
 
 
 void configTIM6(void)
@@ -7,18 +7,20 @@ void configTIM6(void)
   // Out Clock is 50MHz
   // Get Interrupt at every 1 sec.
   // it is developer reposibility to clear the interrupt in status Register.
+	
+ // 1: Enable Peripherial Clock of Timer 
+ // RCC->APB1ENR |= SBIT(4);// TIM 6  Enable Periphherial clock
   
 	TIM6->PSC = 4999;
 	TIM6->ARR = 5000;
-	TIM6->CR1 |= SBIT(0);
+	TIM6->CR1 |= SBIT(0);// Start Timer 6
   
   while(1)
   {
-    if((CBIT(TIM6->SR,0)))
-		{
-			TIM6->SR &= RBIT(0);
-			GPIOD->ODR ^= SBIT(12);
-		}
+    if((CBIT(TIM6->SR,0))) {// bit will set every 1 sec
+	TIM6->SR &= RBIT(0);
+	GPIOD->ODR ^= SBIT(12);
+        }
   }
   
 }
